e6ab5ee6703ffed97b5203d5a8fc36e422477d01
HyeongChank <jsk381988@gmail.com>
2023-07-26 16:06:00 +0900

    update

--- date_project/practice/lib/screen/home_screen.dart
+++ date_project/practice/lib/screen/home_screen.dart
@@ -11,22 +11,46 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink[200],
+      backgroundColor: Colors.blue[200],
       body: SafeArea(
+        bottom: false,
         child: Container(
           width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
+              _TopPart(),
+              _BottomPart(),
+            ],
+          )
+
+        ),
+      ),
+    );
+
+  }
+}
+
+class _TopPart extends StatelessWidget {
+  const _TopPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'u%i',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'parisienne',
+              fontSize: 80.0,
+            ),
+          ),
+          Column(
+            children: [
               Text(
-                'u%i',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'parisienne',
-                  fontSize: 80.0,
-                ),
-              ),
-              Text(
-                  '우리 처음 만난 날',
+                '우리 처음 만난 날',
                 style: TextStyle(
                   color: Colors.white,
                   fontFamily: 'sunflower',
@@ -42,26 +66,40 @@ class _HomeScreenState extends State<HomeScreen> {
 
                 ),
               ),
-              IconButton(
-                  onPressed: (){},
-                  icon: Icon(
-                    Icons.favorite,
-                  ),
-              ),
-              Text('D+1',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'sunflower',
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ],
 
+            ],
           ),
-        ),
+          IconButton(
+            iconSize: 60.9,
+            onPressed: (){},
+            icon: Icon(
+              Icons.favorite,
+              color:Colors.red,
+            ),
+          ),
+          Text('D+1',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'sunflower',
+              fontSize: 50.0,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
+
       ),
     );
+  }
+}
 
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Image.asset(
+          'asset/img/middle_image.png'),
+    );
   }
 }
